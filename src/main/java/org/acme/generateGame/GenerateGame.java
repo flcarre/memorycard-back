1203bbadcbd005f4ff2356efd019739f86861989
flcarre <flcarre@student.42.fr>
2021-11-30 21:29:36 +0100

    start first utils function in TDD

---
+++ src/main/java/org/acme/generateGame/GenerateGame.java
@@ -0,0 +1,21 @@
+package org.acme.generateGame;
+
+import io.vertx.core.json.JsonObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("newGame")
+public class GenerateGame {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject newGame() {
+        JsonObject game = new JsonObject();
+        game.put("aa", "bb");
+
+        return game;
+    }
+}
---
+++ src/test/java/org/acme/generateGame/GenerateGameTest.java
@@ -0,0 +1,21 @@
+package org.acme.generateGame;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GenerateGameTest {
+
+    @Test
+    public void GenerateGame() {
+        given()
+                .when().get("newGame")
+                    .then()
+                    .statusCode(200)
+                    .body("aa", is("bb"));
+
+    }
+}
